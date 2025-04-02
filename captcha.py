5cbdedaff395c385269eb23278da3c078bad22b1
xmt328 <xmt328@163.com>
2025-04-02 15:21:04 +0800

    combine MihoyoBBSTools and test_nine

--- captcha.py
+++ captcha.py
@@ -1,9 +1,23 @@
-from request import http
+from test_nine.main import get_pic
+
+import json
+
+
+def captcha(gt: str, challenge: str):
+    print("captcha gt:" + gt + " challenge:" + challenge)
+    try:
+        res = get_pic(gt, challenge)
+        datas = json.loads(res.body).get("data", {})
+        if datas.get("result") == "success":
+            return datas.get("validate")
+    except Exception as e:
+        print(f"Error during captcha processing: {e}")
+    return None  # 失败时返回 None
 
 
 def game_captcha(gt: str, challenge: str):
-    return None  # 失败返回None 成功返回validate
+    return captcha(gt, challenge)
 
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+    return captcha(gt, challenge)
