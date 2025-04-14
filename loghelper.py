b60807e6f5b3a8e688a5fc7aec7edc36ea2e7b68
xmt328 <xmt328@163.com>
2025-04-14 09:28:33 +0800

    默认日志级别

--- loghelper.py
+++ loghelper.py
@@ -10,7 +10,7 @@ if os.path.exists(file_path):
     log = logging.getLogger("AutoMihoyoBBS")
 else:
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format='%(asctime)s %(levelname)s %(message)s',
         datefmt='%Y-%m-%dT%H:%M:%S')
     log = logging
